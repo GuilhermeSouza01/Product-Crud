b5550581576ebf53ab3e9cb96be1676f19c21ca2
Guilherme Souza <ghostgui60@gmail.com>
2023-07-05 21:22:46 -0300

    Refactoring to add partials files

--- create.php
+++ create.php
@@ -74,53 +74,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 
-<!doctype html>
-<html lang="en">
+<?php include_once "views/partials/header.php" ?>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<h1>Create new Product</h1>
+<?php if (!empty($errors)) : ?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Producs Crud</title>
-</head>
+    <div class="alert alert-danger">
+        <?php foreach ($errors as $error) : ?>
+            <div><?php echo $error ?></div>
+        <?php endforeach; ?>
+    </div>
+<?php endif ?>
 
-<body>
-    <h1>Create new Product</h1>
-    <?php if (!empty($errors)) : ?>
-
-        <div class="alert alert-danger">
-            <?php foreach ($errors as $error) : ?>
-                <div><?php echo $error ?></div>
-            <?php endforeach; ?>
-        </div>
-    <?php endif ?>
-
-    <form action="" method="post" enctype="multipart/form-data">
-        <div class="mb-3">
-            <label class="form-label">Product Image</label>
-            <br>
-            <input type="file" name="image">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Product Title</label>
-            <input type="text" class="form-control" name="title" value="<?php echo $title ?>">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Product Description</label>
-            <textarea class="form-control" name="description"><?php echo $description ?></textarea>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Product Price</label>
-            <input type="number" steps=".01" class="form-control" name="price" value="<?php echo $price ?>">
-        </div>
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label class="form-label">Product Image</label>
+        <br>
+        <input type="file" name="image">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Product Title</label>
+        <input type="text" class="form-control" name="title" value="<?php echo $title ?>">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Product Description</label>
+        <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Product Price</label>
+        <input type="number" steps=".01" class="form-control" name="price" value="<?php echo $price ?>">
+    </div>
 
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 
 </html>
